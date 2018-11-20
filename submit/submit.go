bebf23ad665ee741cdac2f218ad11d574e52cc32
Brent Pedersen <bpederse@gmail.com>
2018-11-20 09:24:09 -0700

    add -E to bash params

--- submit/submit.go
+++ submit/submit.go
@@ -221,7 +221,7 @@ if [ -f /etc/environment ]; then source /etc/environment; fi
 if [ -f /etc/default/locale ]; then source /etc/default/locale; fi
 set +a
 if [ -f /etc/profile ]; then source /etc/profile; fi
-set -euo pipefail
+set -Eeuo pipefail
 %s
 %s
 %s
